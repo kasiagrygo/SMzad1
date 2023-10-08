9b9da9f3a095ef97a9d950cc67168fbb6d92d408
kasiagrygo <katarzynagrygo@icloud.com>
2023-10-08 21:04:33 +0200

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+f
+6app/src/main/java/com/example/smzad1/MainActivity.java,1\f\1ff80550cc81d3b427b7cb6d9563453920f3135a
+b
+2app/src/main/java/com/example/smzad1/Question.java,0\f\0fc584d2a289692001e402771f021183d66d2c0e
---
+++ app/src/main/java/com/example/smzad1/Question.java
@@ -0,0 +1,22 @@
+package com.example.smzad1;
+
+import java.util.UUID;
+
+public class Question {
+
+    public UUID QuestionId;
+    public int QuestionContent;
+    public boolean QuestionTrue;
+
+    public Question(UUID Id, int Content, boolean IsTrue)
+    {
+        QuestionId = Id;
+        QuestionContent = Content;
+        QuestionTrue = IsTrue;
+    }
+
+    public static Question Create(int Content, boolean IsTrue)
+    {
+        return new Question(UUID.randomUUID(), Content, IsTrue);
+    }
+}
